6a72ef96d74202239a60c4dc8a81f49eef3e1956
xiaofan <1353815633@qq.com>
2018-05-31 16:33:00 +0800

    add menu-controller and menu-view

---
+++ app/scripts/controllers/menu.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('udaciMealsApp')
+  .controller('MenuCtrl', function () {
+    this.id = 'yoman';
+    this.name = 'yoman';
+    this.img = 'yoman.jpg';
+    this.rating = 5;
+  });
