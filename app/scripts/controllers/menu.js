f8800bd1efd5963f9f697406272733a94b4108b2
xiaofan <1353815633@qq.com>
2018-06-01 00:54:38 +0800

    angular services

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -8,27 +8,12 @@
  * Controller of the udaciMealsApp
  */
 angular.module('udaciMealsApp')
-  .controller('MenuCtrl', function () {
-    this.items = [
-      {
-        id: '11',
-        name: 'one',
-        img: 'food01.jpg',
-        rating: 3.1
-      },
-      {
-        id: '22',
-        name: 'two',
-        img: 'food02.jpg',
-        rating: 5.5
-      },
-      {
-        id: '33',
-        name: 'three',
-        img: 'food03.jpg',
-        rating: 6.6
-      }
-    ];
+  .controller('MenuCtrl', ['foodFinder', function (menu) {
+    var vm = this;
+
+    menu.getMenu().then(function (data) {
+      vm.items = data;
+    });
 
     this.increment = function (item) {
       item.rating = ((item.rating * 10) + 1) / 10;
@@ -37,4 +22,4 @@ angular.module('udaciMealsApp')
     this.decrement = function (item) {
       item.rating = ((item.rating * 10) - 1) / 10;
     }; 
-  });
+  }]);
